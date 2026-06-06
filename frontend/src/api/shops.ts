b05b60dc9f4a02673039f089061db0b955d0f501
Thilina Bowatta <bowattatab@gmail.com>
2026-06-06 23:45:41 +0530

    Revert "Feature/compare page"

--- frontend/src/api/shops.ts
+++
@@ -1,6 +0,0 @@
-import apiClient from "./client";
-import type { Shop } from "../types";
-
-export function getShops(): Promise<Shop[]> {
-    return apiClient.get<Shop[]>('/shops').then(response => response.data)
-}
