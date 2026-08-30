53b85a099e8814d2062da1c36df566651757a227
Thilina Bowatta <bowattatab@gmail.com>
2026-08-30 22:16:25 +0530

    minor changes

--- scraper/main.py
+++ scraper/main.py
@@ -7,16 +7,16 @@ from scrapers.computerage_scraper import run_scraper as computerage_scraper
 if __name__ == "__main__":
     print("🚀 Starting PriceTracker scraper...")
 
-    # print("\n🔷 Running Nanotek scraper...")
-    # asyncio.run(nanotek_scraper())
+    print("\n🔷 Running Nanotek scraper...")
+    asyncio.run(nanotek_scraper())
 
     print("\n🔷 Running Chama scraper...")
     chama_scraper()
 
-    # print("\n🔷 Running Game Street scraper...")
-    # gamestreet_scraper()
+    print("\n🔷 Running Game Street scraper...")
+    gamestreet_scraper()
 
-    # print("\n🔷 Running ComputerAge scraper...")
-    # asyncio.run(computerage_scraper())
+    print("\n🔷 Running ComputerAge scraper...")
+    asyncio.run(computerage_scraper())
 
     print("\n✅ All scrapers done")
